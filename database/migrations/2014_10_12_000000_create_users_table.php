bd0868b3223f7907803ed10f9cda1cc14164caf9
Muhammad Jafar <mjafarashadiq712@gmail.com>
2021-02-06 17:42:58 +0700

    update register and login page

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('photo');
+            $table->string('photo')->nullable();
             $table->bigInteger('posts')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
